94682bffaab9576ed2f95dffe3810e214cb7edbc
Foxsunshine <kaihatsu.chien.jan@gmail.com>
2023-05-22 20:57:25 +0900

    section1,third commit

---
+++ 01-spring-boot-overview/06-properties-demo/src/main/java/com/jiangcheng/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,32 @@
+package com.jiangcheng.springboot.demo.mycoolapp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    // expose "/" return "hello world"
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach:" + coachName + ", Team name : " + teamName;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/workout")
+    public String workout() {
+        return "Run a hard 5k";
+    }
+
+}
